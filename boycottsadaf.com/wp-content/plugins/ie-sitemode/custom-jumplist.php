919216ee231643058e6ecac91ce7018e2623ec4d
Tint World Store094 <sarmadashoor1@gmail.com>
2024-04-02 10:36:57 -0700

    uploading clone

---
+++ boycottsadaf.com/wp-content/plugins/ie-sitemode/custom-jumplist.php
@@ -0,0 +1,2 @@
+function wpcom_custom_jumplist_reader(){window.external.msSiteModeCreateJumplist("Recent posts");window.external.msSiteModeAddJumpListItem("Taking Back our Language from \"Liberal zionists\"","https://boycottsadaf.com/2022/04/03/mizrahi-agenda/","https://boycottsadaf.com/wp-content/uploads/2022/12/cropped-logo-512x512-1.png?w=16");}
+try{if(window.external && 'msIsSiteMode' in window.external){if (window.external.msIsSiteMode()){wpcom_custom_jumplist_reader();wpcom_sitemode_comment_notify();}}}catch(e){}
